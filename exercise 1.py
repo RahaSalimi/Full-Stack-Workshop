b3d28fca5054d0c0160a93d294a1f5d5fb1a5e7e
Raha Salimi <80513097+RahaSalimi@users.noreply.github.com>
2022-05-03 19:37:51 -0400

    Create exercise 1.py

---
+++ exercise 1.py
@@ -0,0 +1,16 @@
+
+input1=input("please enter first input: ")
+input2=input("please enter second input: ")
+
+inputs_sum= input1+input2
+inputs_sub=input1-input2
+inputs_multi=input1*input2
+inputs_power=input1**input2
+inputs_division=input1/input2
+
+print("sum of inputs = ",inputs_sum)
+print("sub of inputs = ",inputs_sub)
+print("multiply of inputs = ",inputs_multi)
+print("power of inputs = ", inputs_power)
+print("division of inputs = ", inputs_division)
+
