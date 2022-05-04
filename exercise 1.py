a1f9933d95587e45a0b5063c3ef6b7061d35c9bc
engrfaisal90 <engr.faisal90@gmail.com>
2022-05-03 20:07:35 -0400

    Add files via upload

--- exercise 1.py
+++ exercise 1.py
@@ -1,6 +1,16 @@
+input1=None
+input2=None
+while input1 is None:
+    try :
+        input1=float(input("Enter first number: "))
+    except:
+        print('enter integer or float')
 
-input1=float(input("please enter first input: "))
-input2=float(input("please enter second input: "))
+while input2 is None:
+    try :
+        input2=float(input("Enter second number: "))
+    except:
+        print('enter integer or float')
 
 
 inputs_sum= input1+input2
@@ -15,3 +25,17 @@ print("multiply of inputs = ",inputs_multi)
 print("power of inputs = ", inputs_power)
 print("division of inputs = ", inputs_division)
 
+
+input3=None
+while input3 is None:
+    try:
+        input3=float(input("Enter third number: "))
+    except:
+        print('enter integer or float')
+   
+print("sfinal sum is = ",float(inputs_sum)+input3)
+print("sub of inputs = ",float(inputs_sub)-input3)
+print("multiply of inputs = ",float(inputs_multi)*input3)
+print("power of inputs = ", float(inputs_power)**input3)
+print("division of inputs = ", float(inputs_division)/input3)
+    
