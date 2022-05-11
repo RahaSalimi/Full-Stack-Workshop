7c0617a9625bcc220e03eaafdfa90ab27621fde0
Raha Salimi <80513097+RahaSalimi@users.noreply.github.com>
2022-05-10 20:19:04 -0400

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -14,19 +14,55 @@ while addStudnt == 'yes':
     if addContact == 'no':
         break
 
-#print (students_Dict)
-
-
+# function to search a student
 issearch='yes'
+def search_grade():
+    while issearch == 'yes':
+        search_grade = int(input("plese enter a grade to search: "))
+        for name, grade in students_Dict.items():  
+          if grade == search_grade:
+            print("we found the a student with name of " + name + " for grade: "  + grade)
+          else:
+            print("sorry we couldnt find the grade in  list. please try another grade")   
 
-while issearch == 'yes':
-  search_grade = int(input("plese enter a grade to search: "))
-  for name, grade in students_Dict.items():  
-     if grade == search_grade:
-        print("we found the a student with name of " + name + " for grade: "  + grade)
-     else:
-        print("sorry we couldnt find the grade in  list. please try another grade")   
+        addsarch= input('Would you like to search another grade? (yes or no): ')
+        if addsarch == 'no':
+         break
 
-  addsarch= input('Would you like to search another grade? (yes or no): ')
-  if addsarch == 'no':
-      break
+
+#function to update a student's grade
+def update_grade():
+
+  key = input("please enter student name to update: ")
+  if key in students_Dict.keys():
+        print("student exist,please enter new grade ")
+        new_grade=int(input())
+        students_Dict.update({key:new_grade})
+        print("grade updated =", new_grade)
+  else:
+        print("Student Not Exist")
+
+
+#function to delete a students
+def delete_grade():
+
+  remove_key= input("please enter student name to delete: ")
+  if remove_key in students_Dict.keys():
+	    pop_item= students_Dict.pop(remove_key)
+        print("student {} removed from list",remove_key )
+  else:
+        print("Student Not Exist")
+
+
+
+action=input("what action do you want to take? (search  or update or delete): ")
+
+if action == "search":
+    search_grade()
+elif action == "update":
+    update_grade()
+elif action == "delete":
+    delete_grade() 
+
+
+
