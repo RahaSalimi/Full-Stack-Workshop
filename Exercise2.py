e820fe6e14e62b512776553754843c937ab46084
Raha Salimi <80513097+RahaSalimi@users.noreply.github.com>
2022-05-17 21:06:17 -0400

    session3

--- Exercise2.py
+++ Exercise2.py
@@ -1,50 +1,51 @@
 
-students_Dict={}
-
-addStudnt = 'yes'
 # function to add student
 def add_student():
- while addStudnt == 'yes':
     name = input('Please enter the name of the student: ')
     grade = int(input('Please enter the grade of the student: '))
     students_Dict[name] = [grade ]
     addContact = input('Would you like to add another student? (yes or no): ')
     if addContact == 'no':
-        return students_Dict
+     return students_Dict
 
 
 # function to search a student
-issearch='yes'
-def search_student(dict):
-    while issearch == 'yes':
-        search_name = input("plese enter name of student: ")
-        for name, grade in dict.items():  
-          if name == search_name:
-            print("we found the a student with name of " + search_name + " for grade: "  + grade)
-          else:
+
+def search_student():
+    
+        name = input("plese enter name of student: ")
+        if name in students_Dict.keys():  
+            print("we found a student with name of " + name + " and grade: "  + students_Dict[name])
+        else:
             print("sorry we couldnt find the grade in  list. please try another grade")   
             
         addsarch= input('Would you like to search another grade? (yes or no): ')
         if addsarch == 'no':
-         break
+         return
 
 
 #function to update a student's grade
-isupdate ='yes'
+
 def update_grade():
-  while isupdate =='yes':
+  
     key = input("please enter student name to update: ")
-    if key in students_Dict:
-        print("student exist,please enter new grade ")
-        index=students_Dict.index(key)
-        new_grade=int(input("enter new grade: "))
-        students_Dict.update({"key":new_grade})
-        print("grade updated to: ", new_grade)
+    if key in students_Dict.keys():
+      choices= input("what field do you want to update ? (name or grade) : ")
+      if choices== "name":
+        
+        grade=students_Dict(key)
+        new_name=(input("enter new name: "))
+        students_Dict.pop(key)
+        students_Dict[new_name]=grade
+        print("name successfully updated to: ", new_name)
+      elif choices== "grade":
+        new_grade=input("enter new grade: ")
+        students_Dict[key]=new_grade
 
     else:
         print("Student Not Exist")
 
-    addupdate= input('Would you like to update another grade? (yes or no): ')
+    addupdate= input('Would you like to update more? (yes or no): ')
     if addupdate =='no':
         return students_Dict
  
@@ -61,12 +62,14 @@ def delete_grade():
         
     return students_Dict
 
+students_Dict={}
 while(1):
   action=input("what action do you want to take? (add , search  , update or delete): ")
   if action == "add":
      add_student()
+     print(students_Dict)
   elif action == "search":
-    search_student(students_Dict)
+    search_student()
   elif action == "update":
     update_grade()
   elif action =="delete":
---
+++ Session 3/App.py
@@ -0,0 +1,24 @@
+from unicodedata import name
+from matplotlib.pyplot import cla
+
+
+class Student:
+   def __init__(self, name, email,age ,phone_number,gender,birthdate):
+    self.name = name
+    self.email=email
+    self.age = age
+    self.phone_number=phone_number
+    self.gender=gender
+    self.birthdate=birthdate
+
+   def add(self):
+       print("")
+
+   def edit(self):
+       print("")
+
+   def delete(self):
+       print("")
+    
+   def search(self):
+       print("")
